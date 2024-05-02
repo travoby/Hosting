d33554dc6091f8cb7a8b404c8eefde4eea5ab479
van Sopheaktra <sopheak1013@gmail.com>
2024-05-02 16:53:54 +0700

    develop : remove unnecessary files.

--- scripts/layout/footer-hover.js
+++
@@ -1,7 +0,0 @@
-const footerhover = document.querySelector(".footer-hover-layout");
-fetch("../../components/layout/footer-hover.html")
-.then((res) => res.text())
-.then((data) => {
-  footerhover.innerHTML = data;
-})
-.catch((error) => console.error("Error fetching included file:", error));

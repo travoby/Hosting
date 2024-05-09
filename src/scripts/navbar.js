2c4631edd053a3f8521df4b8e87dd4419062ed3d
van Sopheaktra <sopheak1013@gmail.com>
2024-05-09 18:46:40 +0700

    page : add Javascript to navbar

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -7,46 +7,49 @@ fetch("../layout/navbar.html")
 
     navbar.innerHTML = data;
     const newBtn = document.getElementById("new-btn")
-    console.log(newBtn)
     const listContainerNew = document.getElementById("list-container-new")
     function showContainer() {
-
       listContainerNew.classList.add("top-[6%]")
     }
     function hideContainer() {
 
       listContainerNew.classList.remove("top-[6%]")
     }
-    newBtn.addEventListener('mouseenter', showContainer)
+    newBtn.addEventListener('mouseover', showContainer)
+    listContainerNew.addEventListener('mouseenter', showContainer)
+
     listContainerNew.addEventListener('mouseleave', hideContainer)
+    newBtn.addEventListener('mouseleave', hideContainer)
 
-
-    const MenBTN = document.querySelector("#men-btn");
+    
+    const MenBTN = document.getElementById("men-btn");
     const listContainerMen = document.getElementById("list-container-men")
+    
     function showMenContainer() {
-
-      listContainerMen.classList.add("top-[6%]")
+      listContainerMen.classList.add("top-[6%]");
     }
     function hideMenContainer() {
-
-      listContainerMen.classList.remove("top-[6%]")
+      listContainerMen.classList.remove("top-[6%]");
     }
-    MenBTN.addEventListener("mouseenter", showMenContainer)
-    listContainerMen.addEventListener('mouseleave', hideMenContainer)
+    MenBTN.addEventListener("mouseover", showMenContainer)
+    MenBTN.addEventListener("mouseleave", hideMenContainer)
+    listContainerMen.addEventListener("mouseover", showMenContainer)
+    listContainerMen.addEventListener("mouseleave", hideMenContainer)
 
 
-    const WomenBTN = document.querySelector("#women-btn");
+    const WomenBTN = document.getElementById("women-btn");
     const listContainerWomen = document.getElementById("list-container-women")
     function showWomenContainer() {
-
       listContainerWomen.classList.add("top-[6%]")
     }
     function hideWomenContainer() {
-
       listContainerWomen.classList.remove("top-[6%]")
     }
-    WomenBTN.addEventListener("mouseenter", showWomenContainer)
-    listContainerWomen.addEventListener('mouseleave', hideWomenContainer)
+    WomenBTN.addEventListener("mouseover", showWomenContainer)
+    WomenBTN.addEventListener("mouseleave", hideWomenContainer)
+    listContainerWomen.addEventListener("mouseover", showWomenContainer)
+    listContainerWomen.addEventListener("mouseleave", hideWomenContainer)
+
 
 
     const KidBTN = document.querySelector("#kid-btn");
@@ -60,12 +63,17 @@ fetch("../layout/navbar.html")
       listContainerKid.classList.remove("top-[6%]")
     }
 
-    KidBTN.addEventListener("mouseenter", showKidContainer)
+    KidBTN.addEventListener("mouseover", showKidContainer)
     listContainerKid.addEventListener('mouseleave', hideKidContainer)
+    KidBTN.addEventListener("mouseleave", hideKidContainer)
+    listContainerKid.addEventListener('mouseover', showKidContainer)
+
 
 
     const SaleBTN = document.querySelector("#sale-btn");
     SaleBTN.addEventListener("mouseover", showContainer)
+    SaleBTN.addEventListener("mouseleave", hideContainer)
+    
 
     // Humberger
 

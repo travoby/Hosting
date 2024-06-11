b0d03fc7313200d9f44703745ea69dd0a52ec85c
sopheaktra <sopheak1013@gmail.com>
2024-06-11 16:12:22 +0700

    hotfix: fix on caraousal member

--- src/scripts/components/class.js
+++ src/scripts/components/class.js
@@ -1,38 +1,66 @@
-// Classic Soprt slider
-export const classic = document.querySelector(".classic-layout");
+// // Classic Soprt slider
+// export const classic = document.querySelector(".classic-layout");
+//   fetch("../layout/classic.html")
+//   .then((res) => res.text())
+//   .then((data) => {
+//     classic.innerHTML = data;
+
+//     const pBtn = document.querySelector("#pBtn");
+//     const nBtn = document.querySelector("#nBtn");
+//     const classCard = document.querySelector("#classCard");
+//     const classImage = document.querySelectorAll("#classImage");
+
+//     currentIndex = 0;
+
+//     function classNext(){
+//         if (currentIndex < 2){
+//             currentIndex = (currentIndex + 1) % classImage.length;
+//             classupdateSlider()
+//         }
+//     }
+//     function classPrev() {
+//         if (currentIndex > 0) {
+//             currentIndex = (currentIndex - 1 + classImage.length) % classImage.length;
+//             classupdateSlider()
+            
+//         }
+//     }
+
+//     function classupdateSlider() {
+//         const offset = -currentIndex * 285; // Adjust this value as needed
+//         classCard.style.transform = `translateX(${offset}px)`;
+//     }
+//     pBtn.addEventListener("click", classPrev)
+//     nBtn.addEventListener("click", classNext)
+//   })
+//   .catch((error) => console.error("Error fetching included file:", error));
+
+
+  export const classic = document.querySelector(".classic-layout");
   fetch("../layout/classic.html")
   .then((res) => res.text())
   .then((data) => {
     classic.innerHTML = data;
 
-    const pBtn = document.querySelector("#pBtn");
-    const nBtn = document.querySelector("#nBtn");
-    const classCard = document.querySelector("#classCard");
-    const classImage = document.querySelectorAll("#classImage");
-
-    currentIndex = 0;
-
-    function classNext(){
-        if (currentIndex < 2){
-            currentIndex = (currentIndex + 1) % classImage.length;
-            classupdateSlider()
-        }
+    const preBtn = document.querySelector("#previous");
+    const nextBtn = document.querySelector("#next");
+    const classContainer = document.getElementById(".classCard");
+    
+    function scrollClassNext(){
+        classContainer.scrollBy({
+            left: 250,
+            behavior: "smooth",
+          });
     }
-    function classPrev() {
-        if (currentIndex > 0) {
-            currentIndex = (currentIndex - 1 + classImage.length) % classImage.length;
-            classupdateSlider()
-            
-        }
+    function scrollClassBack(){
+        classContainer.scrollBy({
+            left: -250,
+            behavior: "smooth",
+          });
     }
-
-    function classupdateSlider() {
-        const offset = -currentIndex * 285; // Adjust this value as needed
-        classCard.style.transform = `translateX(${offset}px)`;
-    }
-    pBtn.addEventListener("click", classPrev)
-    nBtn.addEventListener("click", classNext)
+    
+    preBtn.addEventListener("click",scrollClassBack)
+    nextBtn.addEventListener("click",scrollClassNext)
+   
   })
   .catch((error) => console.error("Error fetching included file:", error));
-
-
--- src/scripts/components/member.js
+++ src/scripts/components/member.js
@@ -1,40 +1,30 @@
-// Member Benefit slider
+
+//Member
 export const member = document.querySelector(".member-layout");
   fetch("../layout/member.html")
   .then((res) => res.text())
   .then((data) => {
     member.innerHTML = data;
 
-    // Members
-    const memCardContainer = document.getElementById("memCard");
-    const memImages = document.querySelectorAll("#memImage");
-    const memPreBtn = document.getElementById("memPreBtn");
-    const memNextBtn = document.getElementById("memNextBtn");
-    currentIndex = 0;
-
-    function memNext() {
-        if (currentIndex < 1) {
-            currentIndex = (currentIndex + 1) % memImages.length;
-            Slider()
-        }
-
+    const memberPre = document.querySelector("#memPreBtn");
+    const memberNext = document.querySelector("#memNextBtn");
+    const memberscrollpop = document.querySelector("#memCard")
+    
+    function scrollMemNext(){
+        memberscrollpop.scrollBy({
+            left: 250,
+            behavior: "smooth",
+          });
     }
-
-    function memPrev() {
-        if (currentIndex > 0) {
-            currentIndex = (currentIndex - 1 + memImages.length) % memImages.length;
-            Slider()
-        }
+    function scrollMemBack(){
+        memberscrollpop.scrollBy({
+            left: -250,
+            behavior: "smooth",
+          });
     }
-
-    function Slider() {
-        const offset = -currentIndex * 560; // Adjust this value as needed
-        memCardContainer.style.transform = `translateX(${offset}px)`;
-    }
-
-    memPreBtn.addEventListener("click",  memPrev)
-    memNextBtn.addEventListener("click", memNext)
+    
+    memberPre.addEventListener("click",scrollMemBack)
+    memberNext.addEventListener("click", scrollMemNext)
+   
   })
   .catch((error) => console.error("Error fetching included file:", error));
-
-

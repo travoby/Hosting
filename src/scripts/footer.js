f67137915c9442d3e87aaa048c9327bede033f96
sopheaktra <sopheak1013@gmail.com>
2024-06-11 13:50:12 +0700

    hotfix: remove unuse src file

--- src/scripts/footer.js
+++
@@ -1,7 +0,0 @@
-const footer = document.querySelector(".footer-layout");
-fetch("../layout/footer.html")
-.then((res) => res.text())
-.then((data) => {
-  footer.innerHTML = data;
-})
-.catch((error) => console.error("Error fetching included file:", error));
